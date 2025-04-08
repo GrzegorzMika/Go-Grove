d10b9eeb604adbb8d6c30d86241bfd2c9b225e87
GrzegorzMika <g.w.mika@gmail.com>
2025-04-08 23:29:47 +0200

    initialize host

--- examples/led_v1_3.go
+++ examples/led_v1_3.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"time"
 
 	"github.com/GrzegorzMika/Go-Grove/grove"
 	"github.com/gSpera/morse"
+	"periph.io/x/host/v3"
 )
 
 var MESSAGE = "Hello, World!"
@@ -30,6 +32,18 @@ func morseToLED(morseCode string, led *grove.LEDv1_3) {
 }
 
 func main() {
+	state, err := host.Init()
+	if err != nil {
+		fmt.Println("Error initializing host:", err)
+	}
+	if len(state.Failed) > 0 {
+		errs := make([]error, len(state.Failed))
+		for i, f := range state.Failed {
+			errs[i] = f.Err
+		}
+		fmt.Println("Errors initializing host:", errors.Join(errs...))
+	}
+
 	led, err := grove.NewLEDv1_3(22)
 	if err != nil {
 		fmt.Println("Error initializing LED:", err)
